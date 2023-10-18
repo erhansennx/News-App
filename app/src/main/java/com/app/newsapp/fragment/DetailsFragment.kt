d632da7c9087eef5ecf49a6665df9630398e3386
erhansennx <erhanfatih33@gmail.com>
2023-10-18 19:02:09 +0300

    Added navigationUp to Fragment and updated details design.

--- app/src/main/java/com/app/newsapp/fragment/DetailsFragment.kt
+++ app/src/main/java/com/app/newsapp/fragment/DetailsFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.app.newsapp.R
 import com.app.newsapp.databinding.FragmentDetailsBinding
@@ -25,6 +26,15 @@ class DetailsFragment : Fragment() {
         }
 
         return fragmentDetailsBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        fragmentDetailsBinding.backButton.setOnClickListener {
+            findNavController().navigateUp()
+        }
+
     }
 
 }
